353db3dc4bb8dbf619e8e7bf89d7d80b7bdb79da
sunzhengyu <sunzhengyu@tuzhanai.com>
2019-09-17 19:15:13 +0800

    feature: java lang ref

---
+++ src/test/java/com/szy/lamda/lang/ref/TestRef.java
@@ -0,0 +1,16 @@
+package com.szy.lamda.lang.ref;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈 测试jdk-lang ref包 〉
+ *
+ * @author sunzhengyu
+ * @create 2019/9/17
+ * @since 1.0.0
+ */
+public class TestRef {
+
+    public void testRef() {
+
+    }
+}
